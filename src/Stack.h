223c50c692c5bd60668fb2728ee6dcb5a38bbc03
Fikry116 <4nggaza@gmail.com>
2025-11-25 19:05:44 +0700

    Co-authored-by: 06Farasha05 <06Farasha05@users.noreply.github.com>

--- src/Stack.h
+++ src/Stack.h
@@ -1,1 +1,15 @@
 //struct stack
+struct Node {
+    int val;
+    Node* next;
+    
+};
+
+struct Stack {
+    Node* Top;
+    
+};template <class T>
+class Stacka {
+    T val;
+    Stacka<T> *head, *tail;
+};
