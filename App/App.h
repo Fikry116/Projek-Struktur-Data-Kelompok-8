d42916872473e16a22cfea7957247af4f448d22f
Fikry116 <4nggaza@gmail.com>
2025-11-29 23:28:31 +0700

    Brush tool 90% jadi euy

--- App/App.h
+++ App/App.h
@@ -14,170 +14,168 @@ enum class AppState {MainMenu, BFS, DFS};
 
 struct App {
 private:
-    sf::RenderWindow window;
-    sf::Font font;
-    sf::Text DFS;
-    sf::Text BFS;
-    sf::SoundBuffer buffer;
-    sf::Sound menuSound;
-    AppState current_app;
-    CellModifier brush_mod;
+  sf::RenderWindow window;
+  sf::Font font;
+  sf::Text DFS;
+  sf::Text BFS;
+  sf::SoundBuffer buffer;
+  sf::Sound menuSound;
+  AppState current_app;
+  CellModifier brush_mod;
 
-    bool Run;
-
-    Grid myGrid;
+  Grid myGrid;
 public:
-     /**
-      * @brief Konstruktor untuk applikasi yang menginisialisasikan semua member struct App
-      * @param width lebar window
-      * @param height tinggi window
-      */
-    App(unsigned int width, unsigned int height)
-      : window(sf::VideoMode({width, height}), "GG"), 
-        DFS(font, "DFS"), BFS(font, "BFS"), menuSound(buffer), myGrid(1, 5, width, height){
-          if(!font.openFromFile("ARIAL.TTF")) {
-              std::cerr << "File not found\n";
-          }
-
-          if(!buffer.loadFromFile("MenuSoundClick.mp3")) {
+    /**
+    * @brief Konstruktor untuk applikasi yang menginisialisasikan semua member struct App
+    * @param width lebar window
+    * @param height tinggi window
+    */
+  App(unsigned int width, unsigned int height)
+    : window(sf::VideoMode({width, height}), "GG"), 
+      DFS(font, "DFS"), BFS(font, "BFS"), menuSound(buffer), myGrid(1, 5, width, height){
+        if(!font.openFromFile("ARIAL.TTF")) {
             std::cerr << "File not found\n";
+        }
+
+        if(!buffer.loadFromFile("MenuSoundClick.mp3")) {
+          std::cerr << "File not found\n";
+        }
+
+        menuSound.setBuffer(buffer);
+
+        BFS.setPosition(sf::Vector2f({width / (2.f+1.f) * 1.f, height / 2.f}));
+        DFS.setPosition(sf::Vector2f({width / (2.f+1.f) * 2.f, height / 2.f}));
+
+        BFS.setFillColor(sf::Color(216, 222, 233));
+        DFS.setFillColor(sf::Color(216, 222, 233));
+
+        current_app= AppState::MainMenu;
+        brush_mod = CellModifier::WallBrush;
+  }
+
+  /**
+   * @brief Menangani interaksi jika menu ditekan
+   * 
+   * Fungsi ini mengecek apakah...... ke tampilan berdasarkan menu yang dipilih (DFS/BFS)
+   * 
+   * @param mouse_pos berisi koordinat mouse x dan y relatif pada window
+   * @return Target AppState untuk transisi tampilan (MainMenu, BFS, DFS)
+   */
+  AppState ClickedMenu(sf::Vector2i mouse_pos) {
+    sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
+    if(DFS.getGlobalBounds().contains(mouse_pos_f)) { //mengecek apakah posisi mouse berada di string DFS
+      return AppState::DFS;
+    }
+    if(BFS.getGlobalBounds().contains(mouse_pos_f)) { //mengecek apakah posisi mouse berada di string BFS
+      return AppState::BFS;
+    }
+    return AppState::MainMenu;
+  }
+
+  /**
+   * @brief Menangani interaksi antara menu dengan kursor mouse
+   * 
+   * Fungsi ini mengecek apakah koordinat kursor mouse berinteraksi dengan menu
+   * (DFS/BFS) yaitu saat kursor berada dijangkauan teks.
+   * ketika berinteraksi maka warna font berganti menjadi warna
+   * dan skala teks berubah pindah ke tampilan berdasarkan menu yang dipilih (DFS/BFS)
+   * 
+   */
+  void CheckMenuCollision(sf::Vector2i mouse_pos) {
+    sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
+    if(DFS.getGlobalBounds().contains(mouse_pos_f)) {
+      if(DFS.getFillColor() == sf::Color(216, 222, 233)) {
+        menuSound.play();
+        DFS.setScale(sf::Vector2f({1.15f, 1.15f}));
+      } 
+      DFS.setFillColor(sf::Color(136, 192, 208));
+      BFS.setFillColor(sf::Color(216, 222, 233));
+      BFS.setScale(sf::Vector2f({1.f, 1.f}));
+      return;
+    }
+    if(BFS.getGlobalBounds().contains(mouse_pos_f)) {
+      if(BFS.getFillColor() == sf::Color(216, 222, 233)) {
+          menuSound.play();
+          BFS.setScale(sf::Vector2f({1.15f, 1.15f}));
+      } 
+      BFS.setFillColor(sf::Color(136, 192, 208));
+      DFS.setFillColor(sf::Color(216, 222, 233));
+      DFS.setScale(sf::Vector2f({1.f, 1.f}));
+      return;
+    }
+    return;
+  }
+
+  //Main loop 
+  void run() {
+    while(window.isOpen()) {
+      while(auto eventOpt = window.pollEvent()) { //looping ketika terjadi event pada window.
+        auto &event = *eventOpt;
+        if (auto *resize = event.getIf<sf::Event::Resized>()) { //update ukuran window kalau di resize
+          sf::View view(sf::FloatRect({0.f, 0.f}, 
+          {static_cast<float>(resize->size.x), static_cast<float>(resize->size.y)}));
+              window.setView(view); //mengatur ukuran baru window.
+          //update posisi teks BFS dan DFS
+          BFS.setPosition({resize->size.x / (2.f+1.f)* 1.f, resize->size.y/(2.f)});  
+          DFS.setPosition({resize->size.x / (2.f+1.f)* 2.f, resize->size.y/(2.f)});  
+          
+        } else if (event.getIf<sf::Event::Closed>()) { //event window ditutup
+          window.close();
+        } else if (event.is<sf::Event::MouseButtonPressed>()) { //event kalau mouse diklik
+          sf::Vector2i mouse_pos = sf::Mouse::getPosition(window); //mengambil posisi mouse
+          if(current_app == AppState::MainMenu) {
+              current_app = ClickedMenu(mouse_pos); 
+          } else if(current_app == AppState::DFS || current_app == AppState::BFS) {
+              //fitur untuk brush tool.
+              myGrid.BrushTool(window, brush_mod, mouse_pos);
+              std::cout << "Pressed DFS\n";
           }
+        } else if (event.is<sf::Event::KeyPressed>()) {
+          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Escape) && current_app != AppState::MainMenu) {
+            current_app = AppState::MainMenu;
 
-          menuSound.setBuffer(buffer);
-
-          BFS.setPosition(sf::Vector2f({width / (2.f+1.f) * 1.f, height / 2.f}));
-          DFS.setPosition(sf::Vector2f({width / (2.f+1.f) * 2.f, height / 2.f}));
-
-          BFS.setFillColor(sf::Color(216, 222, 233));
-          DFS.setFillColor(sf::Color(216, 222, 233));
-
-          current_app= AppState::MainMenu;
-          brush_mod = CellModifier::WallBrush;
-          Run = false;
-    }
-
-    /**
-     * @brief Menangani interaksi jika menu ditekan
-     * 
-     * Fungsi ini mengecek apakah...... ke tampilan berdasarkan menu yang dipilih (DFS/BFS)
-     * 
-     * @param mouse_pos berisi koordinat mouse x dan y relatif pada window
-     * @return Target AppState untuk transisi tampilan (MainMenu, BFS, DFS)
-     */
-    AppState ClickedMenu(sf::Vector2i mouse_pos) {
-        sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
-        if(DFS.getGlobalBounds().contains(mouse_pos_f)) { //mengecek apakah posisi mouse berada di string DFS
-            return AppState::DFS;
-        }
-        if(BFS.getGlobalBounds().contains(mouse_pos_f)) { //mengecek apakah posisi mouse berada di string BFS
-            return AppState::BFS;
-        }
-        return AppState::MainMenu;
-    }
-
-    /**
-     * @brief Menangani interaksi antara menu dengan kursor mouse
-     * 
-     * Fungsi ini mengecek apakah koordinat kursor mouse berinteraksi dengan menu
-     * (DFS/BFS) yaitu saat kursor berada dijangkauan teks.
-     * ketika berinteraksi maka warna font berganti menjadi warna
-     * dan skala teks berubah pindah ke tampilan berdasarkan menu yang dipilih (DFS/BFS)
-     * 
-     */
-    void CheckMenuCollision(sf::Vector2i mouse_pos) {
-        sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
-        if(DFS.getGlobalBounds().contains(mouse_pos_f)) {
-            if(DFS.getFillColor() == sf::Color(216, 222, 233)) {
-                menuSound.play();
-                DFS.setScale(sf::Vector2f({1.15f, 1.15f}));
-            } 
-            DFS.setFillColor(sf::Color(136, 192, 208));
-            BFS.setFillColor(sf::Color(216, 222, 233));
-            BFS.setScale(sf::Vector2f({1.f, 1.f}));
-            return;
-        }
-        if(BFS.getGlobalBounds().contains(mouse_pos_f)) {
-            if(BFS.getFillColor() == sf::Color(216, 222, 233)) {
-                menuSound.play();
-                BFS.setScale(sf::Vector2f({1.15f, 1.15f}));
-            } 
-            BFS.setFillColor(sf::Color(136, 192, 208));
+            //setting ulang skala dan warna 
             DFS.setFillColor(sf::Color(216, 222, 233));
             DFS.setScale(sf::Vector2f({1.f, 1.f}));
-            return;
+            BFS.setFillColor(sf::Color(216, 222, 233));
+            BFS.setScale(sf::Vector2f({1.f, 1.f}));
+          } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
+              brush_mod = CellModifier::WallBrush;
+          } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) {
+              brush_mod = CellModifier::SetStart;
+          } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::E)) {
+              brush_mod = CellModifier::SetEnd;
+          }
+        } else if (event.is<sf::Event::MouseMoved>()) {
+          sf::Vector2i mouse_pos = sf::Mouse::getPosition(window); //mengambil posisi mouse
+          if(current_app == AppState::MainMenu) CheckMenuCollision(mouse_pos);
         }
-        return;
-    }
+      }
+      
+      window.clear(sf::Color(15, 23, 42));
 
-    //Main loop 
-    void run() {
-        while(window.isOpen()) {
-            while(auto eventOpt = window.pollEvent()) { //looping ketika terjadi event pada window.
-                auto &event = *eventOpt;
-                if (auto *resize = event.getIf<sf::Event::Resized>()) { //update ukuran window kalau di resize
-                    sf::View view(sf::FloatRect({0.f, 0.f}, 
-                    {static_cast<float>(resize->size.x), static_cast<float>(resize->size.y)}));
-                        window.setView(view); //mengatur ukuran baru window.
-                    //update posisi teks BFS dan DFS
-                    BFS.setPosition({resize->size.x / (2.f+1.f)* 1.f, resize->size.y/(2.f)});  
-                    DFS.setPosition({resize->size.x / (2.f+1.f)* 2.f, resize->size.y/(2.f)});  
-                    
-                } else if (event.getIf<sf::Event::Closed>()) { //event window ditutup
-                    window.close();
-                } else if (event.is<sf::Event::MouseButtonPressed>()) { //event kalau mouse diklik
-                    sf::Vector2i mouse_pos = sf::Mouse::getPosition(window); //mengambil posisi mouse
-                    if(current_app == AppState::MainMenu) {
-                        current_app = ClickedMenu(mouse_pos); 
-                    } else if(current_app == AppState::DFS || current_app == AppState::BFS) {
-                        //fitur untuk brush tool.
-                        myGrid.BrushTool(window, brush_mod, mouse_pos);
-                    }
-                } else if (event.is<sf::Event::KeyPressed>()) {
-                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::Escape) && current_app != AppState::MainMenu) {
-                        current_app = AppState::MainMenu;
-
-                        //setting ulang skala dan warna 
-                        DFS.setFillColor(sf::Color(216, 222, 233));
-                        DFS.setScale(sf::Vector2f({1.f, 1.f}));
-                        BFS.setFillColor(sf::Color(216, 222, 233));
-                        BFS.setScale(sf::Vector2f({1.f, 1.f}));
-                    } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
-                        brush_mod = CellModifier::WallBrush;
-                    } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) {
-                        brush_mod = CellModifier::SetStart;
-                    } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::E)) {
-                        brush_mod = CellModifier::SetEnd;
-                    }
-                } else if (event.is<sf::Event::MouseMoved>()) {
-                    sf::Vector2i mouse_pos = sf::Mouse::getPosition(window); //mengambil posisi mouse
-                    if(current_app == AppState::MainMenu) CheckMenuCollision(mouse_pos);
-                }
-            }
-            
-            window.clear(sf::Color(46, 52, 64));
-
-            switch (current_app) {
-                case AppState::MainMenu: //ketika app state adalah main menu, tampilkan main menu
-                    window.draw(DFS);
-                    window.draw(BFS);
-                    break;
-                case AppState::BFS: //ketika app state adalah BFS, tampilkan BFS
-                    // if (!Run) {
-                        myGrid.DrawGrid(window);
-                        // Run = true;
-                    // }
-                    // std::cout << "BFS\n";
-                    // window.draw(sf::RectangleShape(sf::Vector2f({200, 300})));
-                    break;
-                case AppState::DFS: //ketika app state adalah DFS, tampilkan DFS
-                    // std::cout << "DFS\n";
-                    // myGrid.DrawGrid(window);
-                    break;
-                default:
-                    break;
-            }
+      switch (current_app) {
+        case AppState::MainMenu: //ketika app state adalah main menu, tampilkan main menu
+          window.draw(DFS);
+          window.draw(BFS);
+          break;
+        case AppState::BFS: //ketika app state adalah BFS, tampilkan BFS
+            // if (!Run) {
+          myGrid.DrawGrid(window);
+                // Run = true;
+            // }
+            // std::cout << "BFS\n";
+            // window.draw(sf::RectangleShape(sf::Vector2f({200, 300})));
+          break;
+        case AppState::DFS: //ketika app state adalah DFS, tampilkan DFS
             // std::cout << "DFS\n";
-            window.display(); //mengupdate tampilan window.
-        }
+            // myGrid.DrawGrid(window);
+          break;
+        default:
+          break;
+      }
+      // std::cout << "DFS\n";
+      window.display(); //mengupdate tampilan window.
     }
+  }
 };
--- src/Grid.h
+++ src/Grid.h
@@ -11,88 +11,114 @@ enum class CellState {
     InStack,
     Visited,
     Path,
-    Wall
+    Wall,
+    Start,
+    End
  };
 
- enum class CellModifier {
+enum class CellModifier {
     WallBrush,
     SetStart,
     SetEnd
 };
 
 struct Grid {
-    int collumn;
-    int row;
-    int TOTAL_GRID;
-    float SIZE_GRID;
-    int start_grid;
-    int end_grid;
-    unsigned int window_width;
-    unsigned int window_height;
+  int collumn;
+  int row;
+  int TOTAL_GRID;
+  float SIZE_GRID;
+  int start_grid;
+  int end_grid;
+  unsigned int window_width;
+  unsigned int window_height;
     
-    std::vector<std::vector<int>> myGrid;
-    std::vector<CellState> grid_state;
-    std::vector<int> path;
+  std::vector<std::vector<int>> myGrid;
+  std::vector<CellState> grid_state;
+  std::vector<int> path;
     
-    sf::RectangleShape drawable_grid;
+  sf::RectangleShape drawable_grid;
     
-    Grid(int start,int end, unsigned int width, unsigned int height) 
-      : start_grid(start), end_grid(end), window_width(width), window_height(height) {
-        SIZE_GRID = 40.f;
-        collumn = width / SIZE_GRID;
-        row = height / SIZE_GRID;
-        TOTAL_GRID = collumn * row;
-        grid_state.resize(TOTAL_GRID, CellState::Idle);
+  Grid(int start,int end, unsigned int width, unsigned int height) 
+    : start_grid(start), end_grid(end), window_width(width), window_height(height) {
+      SIZE_GRID = 40.f;
+      collumn = width / SIZE_GRID;
+      row = height / SIZE_GRID;
+      TOTAL_GRID = collumn * row;
+      grid_state.resize(TOTAL_GRID, CellState::Idle);
 
-        drawable_grid.setSize(sf::Vector2f({SIZE_GRID-1.f, SIZE_GRID-1.f}));
-        myGrid.resize(TOTAL_GRID);
+      drawable_grid.setSize(sf::Vector2f({SIZE_GRID-1.f, SIZE_GRID-1.f}));
+      myGrid.resize(TOTAL_GRID);
 
+  }
+
+  void DrawGrid(sf::RenderWindow &window) {
+    for(unsigned int i = 0; i < TOTAL_GRID; i++) {
+      
+      CellState state = grid_state[i];
+      sf::Color color;
+      if(state == CellState::Idle) {
+        color = sf::Color(20, 20, 20);
+      } else if (state == CellState::Wall) {
+        color = sf::Color(100, 116, 139);
+      } else if (state == CellState::InQueue || state == CellState::InStack) {
+        color = sf::Color(171, 224, 240);
+      } else if (state == CellState::Visited) {
+        color = sf::Color(6, 182, 212);
+      } else if (state == CellState::Path) {
+        color = sf::Color(250, 204, 21);
+      } else if (state == CellState::Start) {
+        color = sf::Color(34, 197, 94);
+      } else if (state == CellState::End) {
+        color = sf::Color(239, 68, 68);
+      }
+      
+      float posX = (i % collumn) * SIZE_GRID;
+      float posY = (i / collumn) * SIZE_GRID;
+      drawable_grid.setPosition({posX, posY});
+        
+      drawable_grid.setFillColor({color});
+      drawable_grid.setOutlineThickness(-1.f);
+      drawable_grid.setOutlineColor(sf::Color::Black);
+        
+      window.draw(drawable_grid);
     }
+  }
 
-    void DrawGrid(sf::RenderWindow &window) {
-        for(unsigned int i = 0; i < TOTAL_GRID; i++) {
-          
-          CellState state = grid_state[i];
-          sf::Color color;
-          if(state == CellState::Idle) {
-            color = sf::Color(20, 20, 20);
-          } else if (state == CellState::Wall) {
-            color = sf::Color(100, 116, 139);
-          } else if (state == CellState::InQueue || state == CellState::InStack) {
-            color = sf::Color(171, 224, 240);
-          } else if (state == CellState::Visited) {
-            color = sf::Color(6, 182, 212);
-          } else if (state == CellState::Path) {
-            color = sf::Color(0, 255, 156);
-          }
-          
-          float posX = (i % collumn) * SIZE_GRID;
-          float posY = (i / collumn) * SIZE_GRID;
-          std::cout << posX << " " << posY << "\n";
-          drawable_grid.setPosition({posX, posY});
-            
-          drawable_grid.setFillColor({color});
-          drawable_grid.setOutlineThickness(-1.f);
-          drawable_grid.setOutlineColor(sf::Color::Black);
-            
-          window.draw(drawable_grid);
-        }
-    }
+  int GetIndex(int x, int y) {
+    if (x < 0 || x >= collumn || y < 0 || y >= row) return -1;
+    return y * collumn + x;
+  }
 
-    void BrushTool(sf::RenderWindow &window, CellModifier brush, sf::Vector2i mouse_pos) {
-        sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
-        switch (brush) {
-          case CellModifier::WallBrush:
-            
-            break;
-          case CellModifier::SetStart:
-            break;
-          case CellModifier::SetEnd:
-            break;  
-        default:
-            break;
-        }
-    }
+  void BrushTool(sf::RenderWindow &window, CellModifier brush, sf::Vector2i mouse_pos) {
+      sf::Vector2f mouse_pos_f(static_cast<float>(mouse_pos.x), static_cast<float>(mouse_pos.y));
+      int index = GetIndex(mouse_pos.x/SIZE_GRID, mouse_pos.y/SIZE_GRID);
+
+      if(index == -1) return;
+
+      switch (brush) {
+        case CellModifier::WallBrush:
+          if(grid_state[index] == CellState::End || grid_state[index] == CellState::Start) return;
+
+          grid_state[index] = CellState::Wall;
+          break;
+        case CellModifier::SetStart:
+          if(index == start_grid) return;
+
+          grid_state[index] = CellState::Start;
+          grid_state[start_grid] = CellState::Idle;
+          start_grid = index;
+          break;
+        case CellModifier::SetEnd:
+          if(index == end_grid) return;
+
+          grid_state[index] = CellState::End;
+          grid_state[end_grid] = CellState::Idle;
+          end_grid = index;    
+          break;  
+      default:
+          break;
+      }
+  }
 
 
     //Stack myStack
